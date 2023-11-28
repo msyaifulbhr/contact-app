3bceb9b23188fd655c836203692c0e9366c4eaff
msyaifulbhr <syaifulyy@gmail.com>
2023-11-29 01:01:58 +0700

    add contact

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import ContactApp from './components/ContactApp';
+import ContactInput from './components/ContactInput';
 
 import './styles/style.css';
 
 const root = createRoot(document.getElementById('root'));
+root.render(<ContactInput />);
 root.render(<ContactApp />);
